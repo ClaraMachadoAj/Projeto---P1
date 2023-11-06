c8898eec8d24d4fc0752debcf958614325bb46f0
Larissa Santos <lari.santos.sobrinho@gmail.com>
2023-11-06 16:40:33 -0300

    organizando aqui

--- sisEntradaCopia.py
+++ sisEntradaCopia.py
@@ -1,7 +1,7 @@
 import os
 os.system('cls')
 
-categoria=[]
+livros = []
 
 while True:
     pergunta = input("Deseja cadastrar um livro? [S] sim e [N] não. ").upper()
@@ -10,19 +10,32 @@ while True:
         nome = input("Qual o nome do livro?\n").title()
         autor = input("Quem escreveu este livro?\n").title()
         categoria = input("Qual a categoria do livro?\n").title()
-        custo = float(input("Quanto custou este livro?"))
+        custo = float(input("Quanto custou este livro?\n"))
+
+        livro = {
+            'Nome': nome,
+            'Autor': autor,
+            'Categoria': categoria,
+            'Custo': custo
+        }
+
+        livros.append(livro)
 
         print("As informações do(s) livro(s) adicionado(s) são: \n")
-        print("Nome     Autor     Categoria     Custo\n")
-        print(f"{nome}\t\t\{autor}\t\t{categoria}\t\t{custo}\n")
+        print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
+        print(f"{nome}\t\t{autor}\t\t{categoria}\t\t{custo}\n")
         desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
-
-        
 
         if desejo == 'S':
             continue
-        else: 
+        else:
             break
-    
+
     else:
         break
+
+print("\nLista de livros cadastrados:\n")
+print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
+
+for livro in livros:
+    print(f"{livro['Nome']}\t\t{livro['Autor']}\t\t{livro['Categoria']}\t\t{livro['Custo']}\n")
