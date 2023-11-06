74537b8e3a1b73fe5452301ec952f07f8afdc660
Clara Machado <claramachado2005@gmail.com>
2023-11-06 15:21:49 -0300

    Update sisEntrada.py

--- sisEntrada.py
+++ sisEntrada.py
@@ -2,19 +2,23 @@ import os
 os.system('cls')
 
 while True:
-    per=input("Deseja cadastrar um livro? [S] sim e [N] não. ").upper()
+    pergunta = input("Deseja cadastrar um livro? [S] sim e [N] não. ").upper()
 
-    if per == 'S'
-        nome=input("Qual o nome do livro?\n").title()
-        autor=input("Quem escreveu este livro?\n").title()
-        cat=input("Qual a categoria do livro?\n").title()
-        custo=float(input("Quanto custou este livro?"))
+    if pergunta == 'S':
+        nome = input("Qual o nome do livro?\n").title()
+        autor = input("Quem escreveu este livro?\n").title()
+        categoria = input("Qual a categoria do livro?\n").title()
+        custo = float(input("Quanto custou este livro?"))
 
         print("As informações do(s) livro(s) adicionado(s) são: \n")
         print("Nome     Autor     Categoria     Custo\n")
-        print(f"{nome}\t\t\{autor}\t\t{cat}\t\t{custo}")
-    
+        print(f"{nome}\t\t\{autor}\t\t{categoria}\t\t{custo}")
+        desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
 
+        if desejo == 'S':
+            continue
+        else: 
+            break
     
     else:
         break
