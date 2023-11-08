37a5659d75e58dac9c9aff5b5db28a6d8b1af7a7
Clara Machado <claramachado2005@gmail.com>
2023-11-08 11:51:57 -0300

    Update SGBP.py

--- SGBP.py
+++ SGBP.py
@@ -12,33 +12,35 @@ while True:
 
 
    if direcionamento == 'ADICIONAR':
-      livros['Nome'] = nome = input("Qual o nome do livro?\n").title()
+      while True:
+
+         livros['Nome'] = nome = input("Qual o nome do livro?\n").title()
       
       #Fazer um verificador pra ver se o livro já existe no arquivo, p n add o mesmo livro duas vezes!!
 
-      livros['Autor'] = autor = input("Quem escreveu este livro?\n").title()
-      livros['Categoria'] = categoria = input("Qual a categoria do livro?\n").title()
-      livros['Custo'] = custo = float(input("Quanto custou este livro?\n"))
+         livros['Autor'] = autor = input("Quem escreveu este livro?\n").title()
+         livros['Categoria'] = categoria = input("Qual a categoria do livro?\n").title()
+         livros['Custo'] = custo = float(input("Quanto custou este livro?\n"))
 
       
 
-      print("\t\tAs informações do(s) livro(s) adicionado(s) são: \n")
-      print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
-      print(f"{nome}\t\t{autor}\t\t{categoria}\t\t{custo}\n")
-      desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
+         print("\t\tAs informações do(s) livro(s) adicionado(s) são: \n")
+         print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
+         print(f"{livros['Nome']}\t\t{livros['Autor']}\t\t{livros['Categoria']}\t\t{livros['Custo']}\n")
+         desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
 
-      if desejo == 'S':
-         continue
-      else:
-         print(livros)
-         break
+         if desejo == 'S':
+            continue
+         else:
+            print(livros)
+            break
 
    elif direcionamento == 'VISUALIZAR':
       vis(direcionamento)
 
 
    elif direcionamento == 'ATUALIZAR':
-      att(direcionamento)
+      att(_)
 
    elif direcionamento == 'EXCLUIR':
       ex(direcionamento)
