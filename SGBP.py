719824d2a5aaa671aebc694f9397b9dc346b70eb
Clara Machado <claramachado2005@gmail.com>
2023-11-08 10:42:15 -0300

    atualização

--- SGBP.py
+++ SGBP.py
@@ -1,13 +1,12 @@
 import os
 os.system('cls')
 
-
+livros = {'Nome': [], 'Autor': [], 'Categoria': [], 'Custo': []}
 
 
 def add():
    
-
-
+livros{"Nome"}.append()
 
 while True:
 
@@ -21,24 +20,24 @@ while True:
       categoria = input("Qual a categoria do livro?\n").title()
       custo = float(input("Quanto custou este livro?\n"))
 
-        livro = {
+      livro = {
             'Nome': nome,
             'Autor': autor,
             'Categoria': categoria,
             'Custo': custo
         }
 
-        livros.append(livro)
+      livros.append(livro)
 
-        print("\t\tAs informações do(s) livro(s) adicionado(s) são: \n")
-        print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
-        print(f"{nome}\t\t{autor}\t\t{categoria}\t\t{custo}\n")
-        desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
+      print("\t\tAs informações do(s) livro(s) adicionado(s) são: \n")
+      print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
+      print(f"{nome}\t\t{autor}\t\t{categoria}\t\t{custo}\n")
+      desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
 
-        if desejo == 'S':
-            continue
-        else:
-            break
+      if desejo == 'S':
+         continue
+      else:
+         break
 
    elif direcionamento == 'VISUALIZAR':
       vis(direcionamento)
