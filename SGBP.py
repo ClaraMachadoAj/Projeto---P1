877573cbc39466b28422853c41383922c2b7ce31
Clara Machado <claramachado2005@gmail.com>
2023-11-08 10:15:22 -0300

    Ajustes

--- SGBP.py
+++ SGBP.py
@@ -2,25 +2,53 @@ import os
 os.system('cls')
 
 
+
+
+def add():
+   
+
+
+
 while True:
 
    print('Olá Nathália!')
-   direcionamento = input('Que você gostaria de realizar hoje? (adicionar, visualizar, atualizar ou excluir').capitalize()
+   direcionamento = input('Que você gostaria de realizar hoje? (adicionar, visualizar, atualizar ou excluir').upper()
 
 
-   if direcionamento == 'Adicionar':
-      return add(direcionamento)
+   if direcionamento == 'ADICIONAR':
+      nome = input("Qual o nome do livro?\n").title()
+      autor = input("Quem escreveu este livro?\n").title()
+      categoria = input("Qual a categoria do livro?\n").title()
+      custo = float(input("Quanto custou este livro?\n"))
+
+        livro = {
+            'Nome': nome,
+            'Autor': autor,
+            'Categoria': categoria,
+            'Custo': custo
+        }
+
+        livros.append(livro)
+
+        print("\t\tAs informações do(s) livro(s) adicionado(s) são: \n")
+        print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
+        print(f"{nome}\t\t{autor}\t\t{categoria}\t\t{custo}\n")
+        desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
+
+        if desejo == 'S':
+            continue
+        else:
+            break
+
+   elif direcionamento == 'VISUALIZAR':
+      vis(direcionamento)
 
 
-   elif direcionamento == 'Visualizar':
-      return vis(direcionamento)
+   elif direcionamento == 'ATUALIZAR':
+      att(direcionamento)
 
-
-   elif direcionamento == 'Atualizar':
-      return att(direcionamento)
-
-   elif direcionamento == 'Excluir':
-      return ex(direcionamento)
+   elif direcionamento == 'EXCLUIR':
+      ex(direcionamento)
 
    
    
