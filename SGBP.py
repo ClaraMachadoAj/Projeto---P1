26ad1badc708120627280d8ba4bf151838b00565
Clara Machado <claramachado2005@gmail.com>
2023-11-12 17:21:28 -0300

    Edições e testes

--- SGBP.py
+++ SGBP.py
@@ -19,24 +19,31 @@ while True:
 
          livros['Nome'] = nome = input("Qual o nome do livro?\n").title()
       
-      #Fazer um verificador pra ver se o livro já existe no arquivo, p n add o mesmo livro duas vezes!!
-
-         livros['Autor'] = autor = input("Quem escreveu este livro?\n").title()
-         livros['Categoria'] = categoria = input("Qual a categoria do livro?\n").title()
-         livros['Custo'] = custo = float(input("Quanto custou este livro?\n"))
+         for andando_na_biblioteca in livros:
+            if nome == livros['Nome']:
+               print('Este LIvro já existe em sua biblioteca! ')
+               continue
+            else:
+               livros['Autor'] = autor = input("Quem escreveu este livro?\n").title()
+            livros['Categoria'] = categoria = input("Qual a categoria do livro?\n").title()
+            livros['Custo'] = custo = float(input("Quanto custou este livro?\n"))
 
       
 
-         print("\t\tAs informações do(s) livro(s) adicionado(s) são: \n")
-         print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
-         print(f"{livros['Nome']}\t\t{livros['Autor']}\t\t{livros['Categoria']}\t\t{livros['Custo']}\n")
-         desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
+            print("\t\tAs informações do(s) livro(s) adicionado(s) são: \n")
+            print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
+            print(f"{livros['Nome']}\t\t{livros['Autor']}\t\t{livros['Categoria']}\t\t{livros['Custo']}\n")
+            desejo = input('Você deseja continuar adicionando? [S] sim e [N] não. ').upper()
 
-         if desejo == 'S':
-            continue
-         else:
-            print(livros)
-            break
+            if desejo == 'S':
+               continue
+            else:
+               print(livros)
+               break
+
+         
+
+         
 
    elif direcionamento == 'VISUALIZAR':
       vis(direcionamento)
