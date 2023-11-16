9c3ef823b2b7d2050198060094e8986bd72d8495
Clara Machado <claramachado2005@gmail.com>
2023-11-16 11:23:11 -0300

    Update SGBP_Viz.py

--- SGBP_Viz.py
+++ SGBP_Viz.py
@@ -5,6 +5,45 @@ os.system ('cls')
 
 livros = {'Nome': [], 'Autor': [], 'Categoria': [], 'Custo': []}
 
+#CORES
+BLACK  = "\033[30m",
+RED    = "\033[31m",
+GREEN  = "\033[32m",
+YELLOW = "\033[33m",
+BLUE   = "\033[34m",
+PURPLE = "\033[35m",
+CYAN   = "\033[36m",
+WHITE  = "\033[37m",
+
+# background color
+BLACKB  = "\033[40m",
+REDB    = "\033[41m",
+GREENB  = "\033[42m",
+YELLOWB = "\033[43m",
+BLUEB   = "\033[44m",
+PURPLEB = "\033[45m",
+CYANB   = "\033[46m",
+WHITEB  = "\033[47m",
+
+# bold
+B    = "\033[1m",
+BOFF = "\033[22m",
+
+# italics
+I = "\033[3m",
+IOFF = "\033[23m",
+
+# underline
+U = "\033[4m",
+UOFF = "\033[24m",
+
+# invert
+R = "\033[7m",
+ROFF = "\033[27m",
+
+# reset
+RESET  = "\033[0m",
+
 def exibir_livros():
     print("Nome\t\t\tAutor\t\t\tCategoria\t\tCusto\n")
     for i in range(len(livros['Nome'])):
@@ -81,7 +120,7 @@ def visualizar_livros():
         conteudo = arquivo.read()
         print(conteudo)
 
-print('Olá Nathália!')
+print('BLUEOlá Nathália!BLUE')
 
 while True:
     direcionamento = input('O que você gostaria de realizar hoje? [adicionar, visualizar, atualizar, excluir ou sair]: \n').upper()
