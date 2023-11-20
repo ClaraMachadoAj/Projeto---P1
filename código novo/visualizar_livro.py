a71836562c8be66b322e6f4fbc6bb0d766411d10
ogcaze <mvco@cesar.school>
2023-11-20 19:34:31 -0300

    Update visualizar_livro.py

--- código novo/visualizar_livro.py
+++ código novo/visualizar_livro.py
@@ -5,11 +5,12 @@ from selecionar_linha import selecionar_linha
 
 biblioteca  = 'biblioteca.txt'
 
-def visualizar_livro(arquivo,generos):
-  per = input("O que você deseja visualizar? [geral, genero, favoritos] ").upper()
 
-  arquivo = open('biblioteca.txt', 'r', encoding='utf-8')
+def visualizar_livro(generos):
+  per = input("O que você deseja visualizar? [geral, genero, favoritos] ").upper()
     
+  arquivo = open('biblioteca.txt', 'r', encoding='utf-8')
+
   if per == 'GERAL':
       print(arquivo.read())
       
@@ -24,7 +25,6 @@ def visualizar_livro(arquivo,generos):
     arquivo = open(biblioteca, 'r', encoding = 'utf8') 
     selecionar_linha(arquivo, escolha)
 
-      
       
       
   elif per == 'FAVORITOS':
