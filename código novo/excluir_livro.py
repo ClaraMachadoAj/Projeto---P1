ac599bae8fb76753a5ed23f0f52dfedb5532cc07
Clara Machado <claramachado2005@gmail.com>
2023-11-21 20:31:26 -0300

    Excluir feito

--- código novo/excluir_livro.py
+++ código novo/excluir_livro.py
@@ -1,8 +1,28 @@
 import os
-os.systam ('cls')
+os.system ('cls')
 
-def excluir_livro():
-    excluir=input("Qual livro você deseja excluir? ")
-    arquivo = open('biblioteca.txt', 'a', encoding='utf-8')
-    arquivo.readlines()
-    
+def excluir_livros():
+  escolha = input('Qual livro você deseja excluir? ').upper()
+
+  arquivo = open('biblioteca.txt', 'r', encoding='utf-8')
+
+  linhas = arquivo.readlines()
+  
+
+  linhas_filtradas = [linha for linha in linhas if escolha in linha]
+
+  if not linhas_filtradas:
+       print('Esse livro não existe em sua biblioteca')
+  else:
+       for linha in linhas_filtradas:
+         linhas.remove(linha)
+       arquivo = open('biblioteca.txt', 'w', encoding='utf-8')
+
+       arquivo.writelines(linhas)
+
+       arquivo.close()
+
+       print(f'O livro {escolha} foi excluído com sucesso')
+
+
+excluir_livros()
