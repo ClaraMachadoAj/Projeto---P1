f220e1da778bf67f7ddee3787370490edc0b52fa
Larissa Santos <lari.santos.sobrinho@gmail.com>
2023-11-21 20:00:13 -0300

    Create excluir_livro.py

---
+++ código novo/excluir_livro.py
@@ -0,0 +1,8 @@
+import os
+os.systam ('cls')
+
+def excluir_livro():
+    excluir=input("Qual livro você deseja excluir? ")
+    arquivo = open('biblioteca.txt', 'a', encoding='utf-8')
+    arquivo.readlines()
+    
