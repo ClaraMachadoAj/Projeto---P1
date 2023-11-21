87c82747e8d2105f702706f1ed0bf70ce7cd4dd2
Larissa Santos <lari.santos.sobrinho@gmail.com>
2023-11-21 11:05:04 -0300

    Update atualizar_nome.py

--- código novo/atualizar_nome.py
+++ código novo/atualizar_nome.py
@@ -1,11 +1,17 @@
-def atualizar_nome(biblioteca):
-  
-  arquivo = open(biblioteca, 'r', encoding = 'utf-8')
-  for linha in arquivo:
-    lista = linha.split(';')
-    
-    
-  
+import os
+os.system ('cls')
+
+def atualizar_nome():
+    nome_antigo = input('Qual o livro que você deseja substituir o nome? ').upper()
+    novo_nome = input('Qual novo nome você quer designar a este livro? ').upper()
+
+    with open(biblioteca, 'r+', encoding='utf-8') as arquivo:
+        dados_Arquivo = arquivo.read()
+        dados_Atualizados = dados_Arquivo.replace(nome_antigo, novo_nome)
+
+        arquivo.seek(0)
+        arquivo.write(dados_Atualizados)
+        arquivo.truncate()
 
 biblioteca = 'biblioteca.txt'
 atualizar_nome(biblioteca)
