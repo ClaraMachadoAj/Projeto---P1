3b9dce0b33977104fab5fce4a443c55e335542d0
Clara Machado <claramachado2005@gmail.com>
2023-11-20 22:59:08 -0300

    tentativas e erros

---
+++ código novo/atualizar_nome.py
@@ -0,0 +1,11 @@
+def atualizar_nome(biblioteca):
+  
+  arquivo = open(biblioteca, 'r', encoding = 'utf-8')
+  for linha in arquivo:
+    lista = linha.split(';')
+    
+    
+  
+
+biblioteca = 'biblioteca.txt'
+atualizar_nome(biblioteca)
--- código novo/main.py
+++ código novo/main.py
@@ -2,9 +2,13 @@ import os
 os.system('cls')
 
 from visualizar_livro import visualizar_livro
+from atualizar_nome import atualizar_nome
 
+livros = {'NOME': [], 'AUTOR': [], 'GENERO': [], 'CUSTO':[], 'FAVORITO': []}
 generos = ['FANTASIA','TERROR', 'ROMANCE', 'SUSPENSE', 'AUTOAJUDA']
 biblioteca = 'biblioteca.txt'
 
 
-visualizar_livro(generos)
+#visualizar_livro(generos)
+
+atualizar_nome(biblioteca)
