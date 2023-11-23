db653aee241753f5300cf42cb0e998b11038560e
Clara Machado <claramachado2005@gmail.com>
2023-11-22 23:15:43 -0300

    O ATUALIZAR TA FUNCIONANDO PORRAAAAAAAAA

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ livros = {'NOME': [], 'AUTOR': [], 'GENERO': [], 'CUSTO':[], 'FAVORITO': []}
 generos = []
 custo_total = 0.0
 
-#FUNÇÕES PRINCIPAIS
-
 # FUNÇÕES PRINCIPAIS
 def adicionar_livro():
     global custo_total
@@ -49,7 +47,7 @@ def adicionar_livro():
       favoritado = input('\nEste livro é um de seus favoritos? [Sim ou Não] ').upper()
 
       try:
-        if favoritado not in ['SIM', 'NÃO']:
+        if favoritado not in ['SIM', 'NÃO', 'NAO']:
             raise ValueError('A resposta deve ser "Sim" ou "Não".')
       except ValueError as erro2:
         print(erro2)
@@ -156,6 +154,51 @@ def excluir_livros():
       print(erro1)
 
 
+def atualizar():
+    nome_antigo = input('Qual nome do livro você deseja alterar? ').upper()
+    encontrado = False
+
+    arquivo = open('biblioteca.txt', 'r', encoding='utf-8')
+    linhas = arquivo.readlines()
+    arquivo.close()
+
+    for i in range(len(linhas)):
+        if nome_antigo in linhas[i]:
+            encontrado = True
+            fav = input("Para modificar um valor de um livro existente digite: MODIFICAR\n"
+                        "Para adicionar ou remover um livro como favorito digite: FAVORITO\n\n").upper()
+            if fav == "MODIFICAR":
+                linha_split = linhas[i].split("; ")
+                choose = input("O que voce deseja alterar: Nome, Autor, Categoria ou Custo\n").upper()
+                
+                if choose == 'NOME':
+                    alteracao = input("Novo nome do livro:").upper()
+                    linha_split[0] = alteracao
+                elif choose == 'AUTOR':
+                    alteracao = input("Nome do novo autor:").upper()
+                    linha_split[1] = alteracao
+                elif choose == 'CATEGORIA':
+                    alteracao = input("Nova categoria do livro:").upper()
+                    linha_split[2] = alteracao
+                elif choose == 'CUSTO':
+                    alteracao = input("Novo valor do livro:").upper()
+                    linha_split[3] = alteracao
+                
+                linhas[i] = "; ".join(linha_split) + '\n'
+
+            elif fav == "FAVORITO":
+                choose_fav = input("Para remover dos FAVORITOS digite: DESFAVORITAR\n"
+                                   "Para adicionar digite: FAVORITAR").upper()
+                if choose_fav == "DESFAVORITAR":
+                    linhas.pop(i)
+                    break
+
+    if not encontrado:
+        print("O livro inserido não foi encontrado na biblioteca")
+    else:
+        arquivo = open('biblioteca.txt', 'w', encoding='utf-8')
+        arquivo.writelines(linhas)
+        arquivo.close()
 #FUNÇÕES PRINCIPAIS
 
 #FUNÇÕES COMPLEMENTARES
